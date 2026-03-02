f2411ec2e4f83ba190c6cad61a57bffd23f5594b
Daniel Desira <desiradaniel2007@gmail.com>
2026-03-02 18:45:49 +0100

    Add AddNotificationToCache implementation

--- DPNS/Repositories/NotificationRepository.cs
+++ DPNS/Repositories/NotificationRepository.cs
@@ -1,10 +1,12 @@
 ﻿using DPNS.Entities;
+using StackExchange.Redis;
 
 namespace DPNS.Repositories
 {
     public interface INotificationRepository
     {
         Task AddNotification(string title, string text, string appUrl, string userEmail);
+        Task AddNotificationToCache(string title, string text, string appUrl, string userEmail);
     }
 
     public class NotificationRepository(DpnsDbContext dbContext) : INotificationRepository
@@ -20,6 +22,22 @@ namespace DPNS.Repositories
                 CreatedAt = DateTime.UtcNow,
             });
             await dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddNotificationToCache(string title, string text, string appUrl, string userEmail)
+        {
+            var notification = new PushNotification
+            {
+                Title = title,
+                Text = text,
+                AppUrl = appUrl,
+                UserEmail = userEmail,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            var muxer = await ConnectionMultiplexer.ConnectAsync("localhost");
+            var db = muxer.GetDatabase();
+            await db.ListLeftPushAsync("notifications", System.Text.Json.JsonSerializer.Serialize(notification));
         }
     }
 }
