6dbb54a411d186fbd8923ab12b007c156216785c
Daniel Desira <desiradaniel2007@gmail.com>
2026-04-08 19:37:29 +0200

    #19: Remove cache notifications method

--- DPNS/Repositories/NotificationRepository.cs
+++ DPNS/Repositories/NotificationRepository.cs
@@ -6,7 +6,6 @@ namespace DPNS.Repositories
     public interface INotificationRepository
     {
         Task AddNotification(string title, string text, string appUrl, string userEmail);
-        Task AddNotificationToCache(string title, string text, string appUrl, string userEmail);
     }
 
     public class NotificationRepository(DpnsDbContext dbContext) : INotificationRepository
@@ -22,22 +21,6 @@ namespace DPNS.Repositories
                 CreatedAt = DateTime.UtcNow,
             });
             await dbContext.SaveChangesAsync();
-        }
-
-        public async Task AddNotificationToCache(string title, string text, string appUrl, string userEmail)
-        {
-            var notification = new PushNotification
-            {
-                Title = title,
-                Text = text,
-                AppUrl = appUrl,
-                UserEmail = userEmail,
-                CreatedAt = DateTime.UtcNow,
-            };
-
-            var muxer = await ConnectionMultiplexer.ConnectAsync("localhost");
-            var db = muxer.GetDatabase();
-            await db.ListLeftPushAsync("notifications", System.Text.Json.JsonSerializer.Serialize(notification));
         }
     }
 }
